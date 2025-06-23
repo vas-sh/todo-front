0bcd905eee928c140de4257ece0868c247aa4802
vas-sh <vas.shopuk@gmail.com>
2025-06-23 20:02:23 +0300

    added autirization after confirmation

--- src/app/confirm/confirm.page.ts
+++ src/app/confirm/confirm.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { UserService } from '../services/user.service';
+import { LoginResp } from '../interfaces/login-resp';
 
 @Component({
   selector: 'app-confirm',
@@ -13,14 +14,18 @@ export class ConfirmPage implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private userService: UserService,
+    private router: Router,
   ) { }
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
-      this.userService.confirm(params["id"]).subscribe((resp: any) => {
-        console.log(resp)
+      this.userService.confirm(params["id"]).subscribe({
+        next: (resp: LoginResp) => {
+          this.userService.storeJwtToken(resp);
+          this.userService.storeUser(resp);
+          this.router.navigate(['/home'])
+        },
       })
     })
   }
-
 }
--- src/app/interfaces/report-completion.ts
+++ src/app/interfaces/report-completion.ts
@@ -1,7 +1,4 @@
-import { Completion } from "../enums/completion";
-
 export interface ReportCompletion {
-    completion: Completion;
     in_time: number;
     not_in_time: number;
     active_overdue: number;
